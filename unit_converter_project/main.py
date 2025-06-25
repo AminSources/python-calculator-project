73c7d9682fea209d545f7fa8090c53db5496b396
Mohammad Amin Farshbaf. <helloamin.com@gmail.com>
2025-06-25 20:33:31 +0330

    python Unit Converter App project added.

---
+++ unit_converter_project/main.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+# ? app main window
+root = Tk()
+root.title("Unit Converter App")
+root.resizable = False
+root.geometry("300x400")
+
+# * app functions
+
+# * app ui
+
+
+# ? main loop
+if __name__ == "__main__":
+    root.mainloop()
