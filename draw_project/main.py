3da7209e4f29c626eadcf1788cf6066ed4322541
Mohammad Amin Farshbaf. <helloamin.com@gmail.com>
2025-06-29 23:34:15 +0330

    rainbow scripts added to draw project.

--- draw_project/main.py
+++ draw_project/main.py
@@ -5,7 +5,7 @@ wn = turtle.Screen()
 skk = turtle.Turtle()
 
 # Set up the turtle's appearance
-skk.speed(1)
+skk.speed(0)
 skk.color("red")
 skk.pensize(3)
 
@@ -19,15 +19,24 @@ skk.pensize(3)
 #     skk.forward(50)
 #     skk.right(120)
 
-# Draw "A"
-skk.left(120)
-skk.forward(100)
-skk.right(240)
-skk.forward(100)
-skk.back(50)
-skk.right(240)
-skk.forward(50)
+# # Draw "A"
+# skk.left(120)
+# skk.forward(100)
+# skk.right(240)
+# skk.forward(100)
+# skk.back(50)
+# skk.right(240)
+# skk.forward(50)
 
+
+colors = ["red", "purple", "blue", "green", "orange", "yellow"]
+t = turtle.Pen()
+turtle.bgcolor("black")
+for x in range(360):
+    t.pencolor(colors[x % 6])
+    t.width(x // 100 + 1)
+    t.forward(x)
+    t.left(59)
 
 turtle.done()
 
